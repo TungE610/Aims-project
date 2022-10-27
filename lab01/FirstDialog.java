597f959b4beccd2662b6c92b17d83cdd4cc1627b
Bui_Danh_Tung <tung.bd200568@sis.hust.edu.vn>
2022-10-27 09:11:57 +0700

    update comment

--- lab01/FirstDialog.java
+++ lab01/FirstDialog.java
@@ -1,8 +1,14 @@
-//Example 2: FirstDialog.java
+// Họ và tên : Bùi Danh Tùng 
+// Mssv : 20200568
+// Ví dụ 2
+// the first dialog Java program
 import javax.swing.JOptionPane;
 public class FirstDialog{
+	
 	public static void main(String[] args) {
-		JOptionPane.showMessageDialog(null, "Hello world! How are you?");
+		
+		JOptionPane.showMessageDialog(null, "Hello world! How are you?");	// In ra chuỗi "Hello world! How are you?"
 		System.exit(0);
+		
 	}
 }
--- lab01/HelloWorld.java
+++ lab01/HelloWorld.java
@@ -1,9 +1,14 @@
-//Example 1: Helloworld.java
+//Ho va ten : Bui Danh Tung
+//Mssv : 20200568
+//Vi du 1
 //Text-printing program
+//Chương trình in ra chuỗi kí tự
 public class HelloWorld {
 	
 	public static void main(String args[]) {
-		System.out.println("Xin chao \n cac ban!");
-		System.out.println("Hello \t world!");
+		
+		System.out.println("Xin chao \n cac ban!"); //In ra chuỗi "Xin chào" và xuống dòng in chuỗi "các bạn!"
+		System.out.println("Hello \t world!"); //In ra chuỗi "Hello" và xuống dòng in chuỗi "world"
+		
 	}
 }
