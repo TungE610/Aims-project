577cea325b95c66a20829a59e0d676b6ea6579b7
Bui_Danh_Tung <tung.bd200568@sis.hust.edu.vn>
2022-10-27 14:11:20 +0700

    update comment

--- lab01/HelloNameDialog.java
+++ lab01/HelloNameDialog.java
@@ -1,10 +1,17 @@
-//Example 3: HelloNameDialog.java
+// Họ và tên: Bùi Danh Tùng
+// Mssv : 20200568
+// Ví dụ 3
+// Print input string program
+// In chuỗi được nhập vào
+
 import javax.swing.JOptionPane;
+
 public class HelloNameDialog{
+	
 	public static void main(String[] args) {
 		String result;
-		result = JOptionPane.showInputDialog("Please enter your name: ");
-		JOptionPane.showMessageDialog(null, "Hi " + result + "!");
+		result = JOptionPane.showInputDialog("Please enter your name: "); // Nhập vào chuỗi và lưu vào chuỗi result
+		JOptionPane.showMessageDialog(null, "Hi " + result + "!");	// In ra chuỗi kết quả
 		System.exit(0);
 	}
 }
--- lab01/ShowTwoNumbers.java
+++ lab01/ShowTwoNumbers.java
@@ -1,20 +1,29 @@
-//Example 5: ShowTwoNumers.java
+// Họ và tên : Bùi Danh Tùng
+// Mssv : 20200568
+// Ví dụ 5
+// Input and print out 2 numbers program
+// Nhập vào và in ra 2 số tuỳ ý
+
 import javax.swing.JOptionPane;
+
+//Nhap vao 2 so va hien ra hai so vua nhap
 public class ShowTwoNumbers {
 	public static void main(String[] args) {
-		String strNum1, strNum2;
-		String strNotification = "You've just entered: ";
+		String strNum1;	//Khai báo xâu để lưu số thứ nhất
+		String strNum2;	//Khai báo xâu để lưu số thứ hai 
+		String strNotification = "You've just entered: "; //Khởi tạo xâu thông báo
 		
 		strNum1 = JOptionPane.showInputDialog(null, "Please input the first number: ", "Input the first number",
-				JOptionPane.INFORMATION_MESSAGE);	
-		strNotification += strNum1 + " and ";
+				JOptionPane.INFORMATION_MESSAGE); // Nhập vào số thứ nhất 
+		
+		strNotification += strNum1 + " and "; // Thêm số thứ nhất vào chuỗi kết quả được in ra
 		
 		strNum2 = JOptionPane.showInputDialog(null, "Please input the second number: ", "Input the second number",
-				JOptionPane.INFORMATION_MESSAGE);
+				JOptionPane.INFORMATION_MESSAGE); // Nhập vào số thứ hai
 				
-		strNotification += strNum2;
+		strNotification += strNum2;	// Thêm số thứ hai vào chuỗi kết quả được in ra
 		
 		JOptionPane.showMessageDialog(null, strNotification, "Show two numbers", JOptionPane.INFORMATION_MESSAGE);
-		System.exit(0);
+		System.exit(0); // In ra 2 số được nhập vào
 	}
 }
