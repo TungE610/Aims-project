c3729fad3d6c7a8e2fb211418a8e1b1505261f5a
Bui_Danh_Tung <tung.bd200568@sis.hust.edu.vn>
2022-11-12 06:55:01 +0700

    update aims in lab02

--- lab02/Aims.java
+++ lab02/Aims.java
@@ -6,9 +6,22 @@ public class Aims {
 		Cart anOrder = new Cart();
 		
 		// create new dvd object and add them to the cart
-		DigitalVideoDsc dvd1 = new DigitalVideoDsc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+		DigitalVideoDsc dvd1 = new DigitalVideoDsc("The Lion King", "Animation", "Roger Allers", 87, 19.95f); 
 		
+		anOrder.addDigitalVideoDisc(dvd1);
 		
+		DigitalVideoDsc dvd2 = new DigitalVideoDsc("Stars War", "Science Fiction", "Geogre Lucas", 87, 24.95f); 
+		
+		anOrder.addDigitalVideoDisc(dvd2);
+		
+		DigitalVideoDsc dvd3 = new DigitalVideoDsc("Aladin", "Animation", "Roger Allers", 18.99f); 
+		
+		anOrder.addDigitalVideoDisc(dvd3);
+		
+		// print total cost of items in the cart
+		
+		System.out.println("Total Cost is: ");
+		System.out.println(anOrder.totalCost);
 		
 	}
 }
